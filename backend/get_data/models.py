99abb341cac4d9dd203aa77fb7c6b8259035465d
promise-temi <temi.promisejohn@gmail.com>
2025-03-05 20:11:54 +0100

    creation de mes models (tables) user, produits, factures. et creation de mon superadmin

--- backend/get_data/models.py
+++ backend/get_data/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+## Create your models here.
+
+
+
+#Table Produits
+class Produits(models.Model):
+    name = models.CharField(max_length=250)
+    prix = models.FloatField()
+
+
+#Table Users
+
+class Users(models.Model):
+    name = models.CharField(max_length=250)
+    adress = models.CharField(max_length=500)
+    type = models.CharField(max_length=10)
+
+
+#Table Factures
+class Factures(models.Model):
+    creation_date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Produits)
+    products_quantity = models.CharField(max_length=250)
+    total_price = models.FloatField()
+
+
