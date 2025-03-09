2ddff8201611de0492ad6ab47bdb141caaa9555d
promise-temi <temi.promisejohn@gmail.com>
2025-03-09 07:31:36 +0100

    j'ai reussi a reccuperer les textes des images pour chaques liens grace à tesseract.js et j'ai stocker tout les texte dans un json qui pourra etre exploité par python

--- get_factures_data/js_modules/tesseractOCR.js
+++ get_factures_data/js_modules/tesseractOCR.js
@@ -1,10 +1,11 @@
-import { createWorker } from 'tesseract.js';
-getTextFromLink('https://projetocrstorageacc.blob.core.windows.net/invoices-2018/FAC_2018_0048-220.png?sv=2019-12-12&ss=b&srt=sco&sp=rl&se=2026-01-01T00:00:00Z&st=2025-01-01T00:00:00Z&spr=https&sig=%2BjCi7n8g%2F3849Rprey27XzHMoZN9zdVfDw6CifS6Y1U%3D')
-async function getTextFromLink(link){
+const { createWorker } = require('tesseract.js'); // ✅ Utiliser require() au lieu de import
+
+async function getTextFromLink(link) {
     const worker = await createWorker('eng');
     const ret = await worker.recognize(link);
-    console.log(ret.data.text.split('\n'));
+    // console.log(ret.data.text.split('\n'));
     await worker.terminate();
+    return ret.data.text.split('\n')
 }
 
-module.exports = getTextFromLink;
+module.exports = getTextFromLink; // ✅ Export correct en CommonJS
